e81d64eeac3a4972ced648cd186204b729e41071
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-10-15 23:58:42 +0300

    update signup.js

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"                                                                                                                                                                                                                                                                                                                                                                                                                                                                             logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
