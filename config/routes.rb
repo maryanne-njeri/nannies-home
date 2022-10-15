73647ce3aed0db2a858a7713b544d5011b550ab8
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-10-15 23:38:20 +0300

    add delete route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
-  post "/signup", to: "users#create"                                                                                                                                                                                                                                                                                                                                                                                                                                                                             logout", to: "sessions#destroy"
+  post "/signup", to: "users#create"
+  delete "/logout", to: "sessions#destroy"                                                                                                                                                                                                                                                                                                                                                                                                                                                                             logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
